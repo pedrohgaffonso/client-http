877724e15a3517e2336821ec4afc55337ef9884c
pedrohgaffonso <pedro.affonso@fatec.sp.gov.br>
2020-05-18 15:33:13 -0300

    RoterLink and routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'bitcoin', component: BitcoinComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
